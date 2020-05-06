b08bd330b04d28326680c2e4a1ecfc4a0abf7ad3
lucasgodeiro <lucas.godeiro@hotmail.com>
2020-05-06 09:26:18 -0300

    - Changes required by CRAN.

--- R/havingIP.R
+++ R/havingIP.R
@@ -3,17 +3,28 @@
 #'
 #' Function to test if the internet connection is available
 #'
+#' @param operational_system The operational system.
 #'
+#' @value A logical vector TRUE if internet connection is available.
+#'
+#' @examples
+#' internet <- havingIP()
 #'
 #' @export
 #'
 #'
-havingIP <- function() {
-  if (.Platform$OS.type == "windows") {
+havingIP <- function(operational_system) {
+
+  if(missing(operational_system)){
+    operational_system <- "windows"
+    }
+
+  if (.Platform$OS.type == operational_system) {
     ipmessage <- system("ipconfig", intern = TRUE)
   } else {
     ipmessage <- system("ifconfig", intern = TRUE)
   }
   validIP <- "((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)[.]){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
-  any(grep(validIP, ipmessage))
+
+  return(any(grep(validIP, ipmessage)))
 }
